b36f9711b219ef6143b1f108e671432d2279b17c
shirasu <atsushi@shira-macbook-pro.local>
2016-09-19 14:30:54 +0900

    [add]パンツのカウント機能
    パンツのカウント機能の追加

--- Assets/Scripts/GameSystemScript.cs
+++ Assets/Scripts/GameSystemScript.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameSystemScript : MonoBehaviour {
+	
+	public int[] pantsCounts;
+
+	public Text[] texts;
+
+	public Canvas uiCanvas;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void AddPantsCount(int index){
+		pantsCounts [index]++;
+		texts [index].text = pantsCounts [index].ToString ();
 	}
 }
--- Assets/Scripts/PantsBehavior.cs
+++ Assets/Scripts/PantsBehavior.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class PantsBehavior : MonoBehaviour {
 
+	public int pantsIndex;
+
+	private GameSystemScript gameSystemScript;
+
 	// Use this for initialization
 	void Start () {
-
+		gameSystemScript = (GameSystemScript)(GameObject.Find("GameSystem").GetComponent("GameSystemScript"));
 	}
 
 	// Update is called once per frame
@@ -16,6 +20,7 @@ public class PantsBehavior : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			Destroy(gameObject);
+			gameSystemScript.AddPantsCount (pantsIndex);
 		}
 	}
 }
