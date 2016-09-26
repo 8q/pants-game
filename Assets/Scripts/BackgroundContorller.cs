9a3029112422c3c8114394f4be4289a91020f459
shirasu <atsushi@shira-macbook-pro.local>
2016-09-27 01:55:48 +0900

    [fix] 背景に入る線の問題
    背景スクロール時につなぎ目のところで謎の線が入る問題、画像の解像度を下げることで解決。

--- Assets/Scripts/BackgroundContorller.cs
+++ Assets/Scripts/BackgroundContorller.cs
@@ -3,22 +3,22 @@ using System.Collections;
 
 public class BackgroundContorller : MonoBehaviour {
 	
-	private GameObject camera;
+	private GameObject mcamera;
 	private Vector3 preCameraPosition;
 	public float scrollSize = 0.1f; 
 
 	// Use this for initialization
 	void Start () {
-		camera = GameObject.FindGameObjectWithTag("MainCamera");
-		preCameraPosition = camera.transform.position;
+		mcamera = GameObject.FindGameObjectWithTag("MainCamera");
+		preCameraPosition = mcamera.transform.position;
 	}
 
 	//背景スクロール
 	// Update is called once per frame
 	void Update () {
-		if (preCameraPosition.x != camera.transform.position.x) {
+		if (preCameraPosition.x != mcamera.transform.position.x) {
 			transform.Translate (-scrollSize, 0, 0);
 		}
-		preCameraPosition = camera.transform.position;
+		preCameraPosition = mcamera.transform.position;
 	}
 }
