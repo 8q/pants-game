5ce70be2a52b0622b6209c6b65ba757ed5b0bf89
shirasu <atsushi@shira-macbook-pro.local>
2016-09-28 00:13:41 +0900

    [add]ドアやその他追加

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorBehaviour : MonoBehaviour {
+
+	public Sprite closeSprite;
+
+	public Sprite openSprite;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().sprite = closeSprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			GetComponent<SpriteRenderer> ().sprite = openSprite;
+		}
+	}
+}
