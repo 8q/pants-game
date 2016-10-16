7a55e590244311a08c1d7008f0655d316a9fae18
G2 <g2simm+FromSourceTree@gmail.com>
2016-10-16 22:11:06 +0900

    [add]START表示用スクリプト追加

---
+++ Assets/Scripts/StartTextBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartTextBehaviour : MonoBehaviour {
+
+    public Text StartText;
+
+
+    // ゲームスタート
+    private IEnumerator GameStart()
+    {
+
+        //        yield return new WaitForSeconds(1f);
+        StartText.text = "START!";
+        yield return new WaitForSeconds(0.4f);
+
+        Debug.Log(Time.deltaTime);
+        int i = 0;
+        for (int fontsize = 200; fontsize < 800; fontsize+=10)
+        {
+            StartText.fontSize = fontsize;
+            StartText.color = new Color(1,1,1,1-((fontsize-200)/500f));
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        StartText.enabled = false;
+
+        yield break;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GameStart());
+    }
+
+#if false
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+#endif
+}
