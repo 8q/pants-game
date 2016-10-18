c00d182c59e76d577e0212962f469a76ed795748
G2 <g2simm+FromSourceTree@gmail.com>
2016-10-19 01:20:55 +0900

    [add]ゴール画面
    ゴール画面を追加。軽微なリファクタリング。5.4.2がダウンロードできなかったのでProjectVersion変わってるけど許して。

--- Assets/Scripts/GameSystemScript.cs
+++ Assets/Scripts/GameSystemScript.cs
@@ -5,86 +5,121 @@ using System.Linq;
 using UnityEngine.SceneManagement;
 using Novel;
 
-public class GameSystemScript : MonoBehaviour {
+public class GameSystemScript : MonoBehaviour
+{
 
-	public bool IsGameOver { get; set; }
+    public bool IsGameOver { get; set; }
 
-	public int[] pantsCounts;
+    public int[] pantsCounts;
 
-	public Canvas uiCanvas;
+    public Canvas uiCanvas;
 
-	public Text[] texts;
+    public Text[] texts;
 
-	public UnityEngine.UI.Image[] zankiImages;
+    public UnityEngine.UI.Image[] zankiImages;
 
-	public int zanki; //現在の残機
+    public int zanki; //現在の残機
 
-	public string sceneName; //このシーンの名前
-
-
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	public void AddPantsCount(int index){
-		pantsCounts [index]++;
-		texts [index].text = pantsCounts [index].ToString ();
-	}
-
-	public void DecreaseZanki(){
-		//ひとまずデバッグ用
-//		if (zanki == 0)
-//			return;
-
-		zanki--;
-		int index = zanki;
-		Destroy (zankiImages[index]);
-		if (zanki == 0) {
-			IsGameOver = true;
-			MoveNextPhase ();
-		}
-	}
+    public string sceneName; //このシーンの名前
 
 
-	//パンツの数で分岐 //GameOverFlagが立っている場合ゲームオーバー
-	public void MoveNextPhase(){
-		if (IsGameOver) {
+    // Use this for initialization
+    void Start()
+    {
+        ///TODO: DEBUG用10秒で強制クリア
+        /*
+        StartCoroutine(this.DelayMethod(10f, () =>
+        {
+            MoveNextPhase();
+        }));
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddPantsCount(int index)
+    {
+        pantsCounts[index]++;
+        texts[index].text = pantsCounts[index].ToString();
+    }
+
+    public void DecreaseZanki()
+    {
+        //ひとまずデバッグ用
+        //		if (zanki == 0)
+        //			return;
+
+        zanki--;
+        int index = zanki;
+        Destroy(zankiImages[index]);
+        if (zanki == 0)
+        {
+            IsGameOver = true;
+            MoveNextPhase();
+        }
+    }
+
+
+    //パンツの数で分岐 //GameOverFlagが立っている場合ゲームオーバー
+    public void MoveNextPhase()
+    {
+        if (IsGameOver)
+        {
 
             //Debug.Log ("GameOver");
 
-            CameraFade.StartAlphaFade(Color.black, false, 0.5f, 0.5f, () => { 
-				GameOverScript.CurrentSceneName = sceneName;
-				SceneManager.LoadScene("gameover"); 
-			});
+            CameraFade.StartAlphaFade(Color.black, false, 0.5f, 0.5f, () =>
+            {
+                GameOverScript.CurrentSceneName = sceneName;
+                SceneManager.LoadScene("gameover");
+            });
             return;
-		} 
+        }
 
-		var maxCount = pantsCounts.Select((val, index) => new { V = val, I = index })
-			.Aggregate((max, working) => (max.V > working.V) ? max : working);
-		switch(maxCount.I){
-		case 0: //あいり
-			Debug.Log ("Airi");
-			NovelSingleton.StatusManager.callJoker("wide/stage1_aft","");
-			break;
-		case 1: //みおん
-			Debug.Log ("Mion");
-			NovelSingleton.StatusManager.callJoker("wide/stage1_aft","");
-			break;
-		case 2: //うみの
-			Debug.Log ("Umino");
-			NovelSingleton.StatusManager.callJoker("wide/stage1_aft","");
-			break;
-		default:
-			Debug.Log ("Move Next Phase Error");
-			break;
-		}
-	}
-		
+
+        // GOALシーンをかぶせる
+        CameraFade.StartAlphaFade(Color.black, false, 0.5f, 0.5f, () =>
+        {
+            SceneManager.LoadScene("goal", LoadSceneMode.Additive);
+        });
+
+        //4.5秒後に実行
+        StartCoroutine(this.DelayMethod(4.5f, () =>
+        {
+            var maxCount = pantsCounts.Select((val, index) => new { V = val, I = index })
+            .Aggregate((max, working) => (max.V > working.V) ? max : working);
+            switch (maxCount.I)
+            {
+                case 0: //あいり
+                    Debug.Log("Airi");
+                    NovelSingleton.StatusManager.callJoker("wide/stage1_aft", "");
+                    break;
+                case 1: //みおん
+                    Debug.Log("Mion");
+                    NovelSingleton.StatusManager.callJoker("wide/stage1_aft", "");
+                    break;
+                case 2: //うみの
+                    Debug.Log("Umino");
+                    NovelSingleton.StatusManager.callJoker("wide/stage1_aft", "");
+                    break;
+                default:
+                    Debug.Log("Move Next Phase Error");
+                    break;
+            }
+
+            // 次の画面をロードしてからGOALシーンを削除
+            CameraFade.StartAlphaFade(Color.black, false, 0.5f, 0.5f, () =>
+            {
+                SceneManager.UnloadScene("goal");
+            });
+
+        }));
+
+    }
+
 
 }
---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoalScript : MonoBehaviour
+{
+
+    public Text[] pantsCountText;
+
+    // Use this for initialization
+    void Start()
+    {
+        var gameSystemScript = (GameSystemScript)(GameObject.Find("GameSystem").GetComponent("GameSystemScript"));
+        for (int i = 0; i < 3; i++)
+        {
+            pantsCountText[i].text = gameSystemScript.pantsCounts[i].ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
