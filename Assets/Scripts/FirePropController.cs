f94ac62cd709369284658ed2b04c70a49fb980d2
shirasu <shira7867@gmail.com>
2016-11-04 02:30:28 +0900

    [mpdified]炎の回転方法
    環境によって炎の速さが異様になるみたいなので、ロジックを変えました

--- Assets/Scripts/FirePropController.cs
+++ Assets/Scripts/FirePropController.cs
@@ -10,12 +10,20 @@ public class FirePropController : MonoBehaviour {
 	void Start () {
 		gameSystemScript = (GameSystemScript)(GameObject.Find("GameSystem").GetComponent("GameSystemScript"));
 		playerController = (PlayerController)(GameObject.FindGameObjectWithTag("Player").GetComponent("PlayerController"));
+		StartCoroutine(this.TimerMethod(0.01f, 
+			() => {},
+			() => {
+				transform.Rotate(new Vector3(0f,0f,1f));
+			},
+			() => false,
+			() => {}
+		));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate(new Vector3(0f,0f,1f));
-	}
+//	// Update is called once per frame
+//	void Update () {
+//		transform.Rotate(new Vector3(0f,0f,1f));
+//	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player" && playerController.IsInvincible == false) {
