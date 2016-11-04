e89ff2a27cc5ed3336776f79cae52555c79377a2
shirasu <shira7867@gmail.com>
2016-11-04 15:39:01 +0900

    [fix]落とし穴の音が重なって聞こえる事がある問題
    一回落とし穴に当たったらプレーヤーの当たり判定取っちゃう作戦

--- Assets/Scripts/OtoshianaBehavior.cs
+++ Assets/Scripts/OtoshianaBehavior.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class OtoshianaBehavior : MonoBehaviour {
 
 	private GameSystemScript gameSystemScript;
+	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		gameSystemScript = (GameSystemScript)(GameObject.Find("GameSystem").GetComponent("GameSystemScript"));
+		player = GameObject.FindGameObjectWithTag ("Player");
+
 	}
 	
 	// Update is called once per frame
@@ -17,6 +20,10 @@ public class OtoshianaBehavior : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
+			Destroy (player.GetComponent<EdgeCollider2D> ());
+			Destroy (player.GetComponent<CircleCollider2D> ());
+			Destroy (player.GetComponent<CircleCollider2D> ());
+			Destroy (player.GetComponent<BoxCollider2D> ());
 			gameSystemScript.IsGameOver = true;
 			gameSystemScript.MoveNextPhase ();
 			SEPlayer.Play (SEPlayer.SE.Otoshiana);
