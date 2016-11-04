7a3b02d8561e194abcaaf02d8481c9aee7352827
shirasu <shira7867@gmail.com>
2016-11-04 19:29:05 +0900

    [add]セーブから復帰できる用のプラグイン
    これで動かなくなったらマジでスミマセン土下座します

---
+++ Assets/JOKER_GAME/Plugins/RouteselectComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Novel{
+	
+	public class RouteselectComponent : AbstractComponent {
+
+		public RouteselectComponent(){
+			//必須項目
+			this.arrayVitalParam = new List<string> {
+				"routename"
+			};
+
+			this.originalParam = new Dictionary<string,string> () {
+				{ "routename","mion" },
+			};
+		}
+
+		public override void start ()
+		{
+			string routeName = this.param ["routename"];
+
+			if ("airi".Equals (routeName)) {
+				Debug.Log("ConstantValues.ROUTE = ConstantValues.RouteName.Airi");
+				ConstantValues.ROUTE = ConstantValues.RouteName.Airi;
+			} else if ("mion".Equals (routeName)) {
+				Debug.Log("ConstantValues.ROUTE = ConstantValues.RouteName.Mion");
+				ConstantValues.ROUTE = ConstantValues.RouteName.Mion;
+			} else if ("umino".Equals (routeName)) {
+				Debug.Log("ConstantValues.ROUTE = ConstantValues.RouteName.Umino");
+				ConstantValues.ROUTE = ConstantValues.RouteName.Umino;
+			} else {
+				Debug.Log("routename invalid value");
+			}
+			//次のシナリオに進む処理
+			this.gameManager.nextOrder ();
+		}
+	}
+}
