c0b2eb13470bb11b0a5341ba1770ef689ae07768
shirasu <shira7867@gmail.com>
2016-12-21 07:31:38 +0900

    [add]サウンドトラック

---
+++ Assets/Scripts/SoundTrackButtonBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SoundTrackButtonBehavior : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	public void OnPointerEnter( PointerEventData eventData )
+	{
+		this.transform.localScale = new Vector3(1.15f, 1.15f, 1.0f);
+	}
+
+	public void OnPointerExit( PointerEventData eventData )
+	{
+		this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+	}
+}
---
+++ Assets/Scripts/SoundTrackMusicButtonBehabior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTrackMusicButtonBehabior : MonoBehaviour {
+
+	private SoundTrackSystemScript soundTrackSystemScript;
+
+	public int musicindex;
+
+	void Start(){
+		soundTrackSystemScript = (SoundTrackSystemScript)(GameObject.Find("SoundTrackSystem").GetComponent("SoundTrackSystemScript"));
+	}
+
+	public void play(){
+		soundTrackSystemScript.MusicIndex = musicindex;
+		soundTrackSystemScript.Play();
+	}
+
+}
