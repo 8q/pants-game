411a455e4dc507990f4b0554ef36bec0c678a221
むさしん <k.inoue@moegi.waseda.jp>
2016-12-21 14:16:48 +0900

    [modified]作業内容をg2のほうに合わせたよ

--- Assets/Scripts/ConstantValues.cs
+++ Assets/Scripts/ConstantValues.cs
@@ -4,19 +4,9 @@ using System.Collections;
 public class ConstantValues  {
 	public static RouteName ROUTE = RouteName.Mion;
 
-	public static ProgressState PROGRESS = ProgressState.Opening;
-
 	public enum RouteName {
 		Airi,
 		Mion,
 		Umino,
-	}
-
-	public enum ProgressState {
-		Opening,
-		Stage1,
-		Stage2,
-		Stage3,
-		Stage4,
 	}
 }
